5087e3ea7d331a80e5f276d03c38783c8441fea5
Topher Brown <topher200@gmail.com>
2015-11-18 04:45:59 -0500

    use unique strings as consts for card bases
    instead of enums. This makes it easier to export the deck state over json

--- card.go
+++ card.go
@@ -9,47 +9,7 @@ type Card struct {
 }
 
 func (c Card) String() string {
-	face := ""
-	switch c.Face {
-	case 0:
-		face = "A"
-	case 1:
-		face = "2"
-	case 2:
-		face = "3"
-	case 3:
-		face = "4"
-	case 4:
-		face = "5"
-	case 5:
-		face = "6"
-	case 6:
-		face = "7"
-	case 7:
-		face = "8"
-	case 8:
-		face = "9"
-	case 9:
-		face = "T"
-	case 10:
-		face = "J"
-	case 11:
-		face = "Q"
-	case 12:
-		face = "K"
-	}
-	suit := ""
-	switch c.Suit {
-	case 0:
-		suit = "♣"
-	case 1:
-		suit = "♦"
-	case 2:
-		suit = "♥"
-	case 3:
-		suit = "♠"
-	}
-	return fmt.Sprintf("%s%s", face, suit)
+	return fmt.Sprintf("%s%s", c.Face, c.Suit)
 }
 
 // GetSignature is the hex representation of the Face and Suit of the card
--- const.go
+++ const.go
@@ -1,34 +1,34 @@
 package deck
 
 // Suit represents the suit of the card (spade, heart, diamon, club)
-type Suit int
+type Suit string
 
 // Face represents the face of the card (ace, two...queen, king)
-type Face int
+type Face string
 
 // Contants for Suit ♠♥♦♣
 const (
-	CLUB Suit = iota
-	DIAMOND
-	HEART
-	SPADE
+	CLUB    Suit = "♣"
+	DIAMOND      = "♦"
+	HEART        = "♥"
+	SPADE        = "♠"
 )
 
 // Contants for Face
 const (
-	ACE Face = iota
-	TWO
-	THREE
-	FOUR
-	FIVE
-	SIX
-	SEVEN
-	EIGHT
-	NINE
-	TEN
-	JACK
-	QUEEN
-	KING
+	ACE   Face = "A"
+	TWO        = "2"
+	THREE      = "3"
+	FOUR       = "4"
+	FIVE       = "5"
+	SIX        = "6"
+	SEVEN      = "7"
+	EIGHT      = "8"
+	NINE       = "9"
+	TEN        = "T"
+	JACK       = "J"
+	QUEEN      = "Q"
+	KING       = "K"
 )
 
 // Global Variables representing the default suits and faces in a deck of cards
