a933ebcfc31937d71f79a37ae64980eaaa790ea9
Topher Brown <topher200@gmail.com>
2015-11-24 15:12:58 -0500

    remove 'random' seeding from library
    I'd rather have my application seed the random library than do
    it in the lower level here

--- shuffle.go
+++ shuffle.go
@@ -1,9 +1,6 @@
 package deck
 
-import (
-	"math/rand"
-	"time"
-)
+import "math/rand"
 
 // MultiShuffle calls Shuffle multipule times
 func (deck *Deck) MultiShuffle(iterations int) {
@@ -15,8 +12,6 @@ func (deck *Deck) MultiShuffle(iterations int) {
 // Shuffle uses Knuth shuffle algo to randomize the deck in O(n) time
 // sourced from https://gist.github.com/quux00/8258425
 func (deck *Deck) Shuffle() {
-
-	rand.Seed(time.Now().UnixNano())
 	N := len(deck.Cards)
 	for i := 0; i < N; i++ {
 		r := i + rand.Intn(N-i)
